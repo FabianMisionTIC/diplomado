b718a7022512eee19974323ad6921273feaee872
FabianMisionTIC <105809646+FabianMisionTIC@users.noreply.github.com>
2022-05-24 17:30:02 -0500

    Update ex2.py

--- ex2.py
+++ ex2.py
@@ -20,3 +20,20 @@ else:
             else:
                 print("The number has more than four digits")
 
+
+num = int(input("Type a integer number: "))
+'''numP = str("The number typed is a postive number")
+numN = str("The number typed is a negative number")'''
+
+if num > 0:
+    if num >= 10 and num <= 99:
+        print("The number typed is a positive integer value and has two digits")
+    else:
+        print("The number typed is a postive integer value and doesnt have two digits")
+else:
+    if num >= -999 and num <= -100:
+        print("The number typed is a negative integer value and has three digits")
+    else:
+        print("The number typed is a negative integer value and doesnt have three digits")
+print(num)
+    
