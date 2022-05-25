da4fcea596c0631e1adc77ba81ad66ec3fe795bf
FabianMisionTIC <105809646+FabianMisionTIC@users.noreply.github.com>
2022-05-25 10:09:30 -0500

    Update exFazt.py

--- exFazt.py
+++ exFazt.py
@@ -1,1 +1,16 @@
+# Important items viewed in Python Course for beginners
 
+'''Datatypes:
+  Strings
+  Numbers:
+    Integers
+    Floats 
+  Lists ()
+  Tuples []
+  Dictionaries { key:value }
+  Sets {}
+Variables =
+Conditionals if, else, elif, and, or, not
+Functions def()
+Loops: While, For in, continue - break
+Modules'''
